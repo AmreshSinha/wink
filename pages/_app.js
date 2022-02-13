6deb899583248d68460cfb8fe430a9c4d3968411
AmreshSinha <theamresh.sinha@gmail.com>
2022-02-13 13:15:34 +0530

    Redo Navbar to Index

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import "../styles/globals.css";
-import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
   );
