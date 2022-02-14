2fc9e057ec657ad9478bde36ee99814ef4e18e08
AmreshSinha <theamresh.sinha@gmail.com>
2022-02-14 18:34:39 +0530

    Add YAML Support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: function (config) {
+    config.module.rules.push(
+      {
+        test: /\.ya?ml$/,
+        type: 'json',
+        use: 'yaml-loader',
+      },
+    )
+    return config
+  }
 }
