ae8d815d34dcf62bd978a02b2c8832a089155ccc
AmreshSinha <theamresh.sinha@gmail.com>
2022-02-15 15:05:42 +0530

    Add Test

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,14 @@
 import Transition from "../components/Transition";
+import TrailingPointerCircle from "../components/TrailingPointerCircle";
 
 export default function Test() {
-    return (
+  return (
+    <>
+      <div style={{width: "100vw", height: "100vh", background: "url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14179/washington.jpg')" }}>
+        <TrailingPointerCircle />
+        <h1>Yo!</h1>
         <Transition loading={true} />
-    )
+      </div>
+    </>
+  );
 }
