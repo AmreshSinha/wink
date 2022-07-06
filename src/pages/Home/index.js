da718d907bd0889f0ed6d3942e11964e06d3baea
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-06 07:36:42 +0000

    Optimized Backgroun

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import styled from "styled-components"
 import Typed from 'typed.js';
 import DesktopNav from "../../components/DesktopNav"
 import ShowMoreButton from "../../components/ShowMoreButton"
-import bgDesktop from "../../images/bgDesktop2.webp"
+import bgDesktop from "../../images/bgDesktop_Optimized.webp"
 import SocialIcons from "../../components/SocialIcons";
 import Cursor from "../../components/Cursor";
 import {Helmet} from "react-helmet";
