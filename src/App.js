290e7d27b9519f87be7249e0ea6c5a530b3fa6bc
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2022-07-06 07:05:48 +0000

    Remove Unused Imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 
 function App() {
