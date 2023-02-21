bdff6821a4d0ac88e4208b85c2225b36be990bd7
AmreshSinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-22 04:39:28 +0530

    Add works

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Me from './pages/Me'
+import Works from './pages/Works'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/works" element={<Works />} />
       <Route path="/blog" element={<Blog />} />
       <Route path="/me" element={<Me />} />
     </Routes>
