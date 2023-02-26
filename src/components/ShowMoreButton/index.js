635e615c1f640d258c6099ff8bf85b8dfbf10fe0
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2023-02-26 15:53:08 +0530

    Add link for works

--- src/components/ShowMoreButton/index.js
+++ src/components/ShowMoreButton/index.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 
 export default function ShowMoreButton() {
     return (
-        <ButtonWrapper to="/">SEE MY WORK</ButtonWrapper>
+        <ButtonWrapper to="/works">SEE MY WORK</ButtonWrapper>
     )
 }
 
