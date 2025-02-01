828e0f1bbcc0b0cfb012c0c4a924bcbd4fd814ac
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2025-02-01 10:43:51 +0530

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -70,7 +70,7 @@ export default function Home() {
         var temp = 0;
         const stringColors = ['#47FFE9', '#00FFAB', '#F7EC09', '#F9D371']
         const typed = new Typed(el.current, {
-            strings: ['developer', 'open sourcer', 'fullstack eng.', 'student'],
+            strings: ['developer', 'open sourcer', 'fullstack eng.'],
             shuffle: false,
             startDelay: 500,
             typeSpeed: 60,
