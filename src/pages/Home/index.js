fbc4c07a93fe32f28efd34258c556fab1b68d756
Amresh Prasad Sinha <35039730+AmreshSinha@users.noreply.github.com>
2025-02-01 10:45:05 +0530

    Update index.js (#8)
    * Update index.js
    
    * Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,10 +22,12 @@ function getWindowSize() {
 
 export default function Home() {
     // Moving Background WIP
-    const movingBackground = useRef()
-    const [windowSize, setWindowSize] = useState(getWindowSize())
-    const [mobile, setMobile] = useState(false)
-    const [tablet, setTablet] = useState(false)
+    const movingBackground = useRef();
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+    const [mobile, setMobile] = useState(false);
+    const [tablet, setTablet] = useState(false);
+    // Current Year
+    const currentYear = new Date().getFullYear().toString();
     useEffect(() => {
         function handleWindowResize() {
             setWindowSize(getWindowSize());
@@ -68,7 +70,7 @@ export default function Home() {
         var temp = 0;
         const stringColors = ['#47FFE9', '#00FFAB', '#F7EC09', '#F9D371']
         const typed = new Typed(el.current, {
-            strings: ['developer', 'open sourcer', 'fullstack eng.', 'student'],
+            strings: ['developer', 'open sourcer', 'fullstack eng.'],
             shuffle: false,
             startDelay: 500,
             typeSpeed: 60,
@@ -250,7 +252,10 @@ export default function Home() {
             <FooterWrapper>
                 {!mobile ? <SocialIcons style={socialIconsAnim} /> : <SocialIcons style={socialIconsAnim} email="amresh@duck.com" />}
                 {/* email={'amresh@duck.com'} */}
-                <Year style={socialIconsAnim}>20<br/>24</Year>
+                <Year style={socialIconsAnim}>
+                    {currentYear.slice(0, 2)}<br/>
+                    {currentYear.slice(2, 4)}
+                </Year>
             </FooterWrapper>
             {!tablet ? <Cursor /> : null}
         </HeroWrapper>
